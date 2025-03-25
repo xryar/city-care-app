022442676c08813269be64ee8008092c54c05e6c
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-03-25 09:34:53 +0700

    big update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -19,6 +19,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   });
   await app.renderPage();
   await registerServiceWorker();
+  console.log('Berhasil mendaftarkan service worker');
 
   window.addEventListener('hashchange', async () => {
     await app.renderPage();
